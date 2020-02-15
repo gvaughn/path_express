3125d8a760533b9b3b2b512a1238d59174ed7209
Greg Vaughn <gvaughn@gmail.com>
2020-02-14 18:07:44 -0600

    ex_doc is in da house

--- mix.exs
+++ mix.exs
@@ -7,9 +7,15 @@ defmodule PathExpress.MixProject do
       version: "0.1.0",
       elixir: "~> 1.9",
       start_permanent: Mix.env() == :prod,
-      deps: []
+      deps: deps()
     ]
   end
 
-  # def application, do: []
+  def application, do: []
+
+  def deps() do
+    [
+      {:ex_doc, "~> 0.21.3", only: :dev, runtime: false}
+    ]
+  end
 end
