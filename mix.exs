4ae92c7add8d64cd360981080ede5de47903a0c7
Greg Vaughn <gvaughn@gmail.com>
2020-02-14 18:14:42 -0600

    metadata

--- mix.exs
+++ mix.exs
@@ -7,11 +7,26 @@ defmodule PathExpress.MixProject do
       version: "0.1.0",
       elixir: "~> 1.9",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      description: description(),
+      package: package(),
+      source_url: "https://github.com/gvaughn/path_express"
     ]
   end
 
   def application, do: []
+
+  def description() do
+    "PathExpress provides increased powers (like nil-safey) from `Kernel.get_in/2` calls"
+  end
+
+  def package() do
+    [
+      maintainers: ["Greg Vaughn"],
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/gvaughn/path_express"}
+    ]
+  end
 
   def deps() do
     [
